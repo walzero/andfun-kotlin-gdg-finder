92979805bc117f326eb2cf2e12f0908e99ef7eab
Walter Rezende <walter-zero@hotmail.com>
2021-01-19 01:43:24 -0300

    #Styling -Removed improper findNavController

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -7,46 +7,46 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
-    private val navigateToGdgListObserver by lazy {
-        Observer<Boolean> {
-            if(findNavController().currentDestination?.id == R.id.home) {
-                findNavController().navigate(R.id.action_home_to_gdgListFragment)
-                viewModel.onNavigatedToSearch()
-            }
-        }
-    }
-
     companion object {
         fun newInstance() = HomeFragment()
     }
 
+    private lateinit var navObserver: Observer<Boolean>
+
     private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         binding.viewModel = viewModel
-        binding.lifecycleOwner = this
 
+        navObserver = Observer<Boolean> { shouldNavigate ->
+            if (shouldNavigate == true) {
+                val navController = binding.root.findNavController()
+                navController.navigate(R.id.action_home_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        }
         return binding.root
     }
 
     override fun onStart() {
         super.onStart()
-        viewModel.navigateToSearch.observe(viewLifecycleOwner, navigateToGdgListObserver)
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, navObserver)
     }
 
     override fun onStop() {
         super.onStop()
-        viewModel.navigateToSearch.removeObserver(navigateToGdgListObserver)
+        viewModel.navigateToSearch.removeObserver(navObserver)
     }
 }
